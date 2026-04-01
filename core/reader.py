b070137902e79a96569714e7afb0a24829195e03
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-01 19:31:28 -0300

    Update reader.py

--- core/reader.py
+++ core/reader.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+def ler_planilha(file):
+    nome = file.name.lower()
+
+    if nome.endswith(".xlsx"):
+        return pd.read_excel(file)
+
+    if nome.endswith(".xls"):
+        return pd.read_excel(file)
+
+    if nome.endswith(".csv"):
+        try:
+            return pd.read_csv(file, sep=";", encoding="utf-8")
+        except:
+            file.seek(0)
+            return pd.read_csv(file, sep=None, engine="python")
+
+    return None
