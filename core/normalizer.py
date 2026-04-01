20590e7d24ccb29f48ba058907d44017758b35ec
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-01 19:32:05 -0300

    Update normalizer.py

--- core/normalizer.py
+++ core/normalizer.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from core.utils import limpar, gerar_codigo_fallback
 
+def normalizar_planilha_entrada(df, base_url, estoque_padrao):
+    out = pd.DataFrame()
+
+    out["Código"] = df.iloc[:,0].astype(str).apply(gerar_codigo_fallback)
+    out["Produto"] = df.iloc[:,1].astype(str).apply(limpar)
+    out["Preço"] = "1.00"
+    out["Estoque"] = estoque_padrao
+    out["Descrição Curta"] = out["Produto"]
+    out["Imagem"] = ""
+    out["Link"] = ""
+    out["Marca"] = ""
+
+    return out
