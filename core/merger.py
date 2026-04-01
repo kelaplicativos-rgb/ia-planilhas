69090d063419adae11e00ed6ec04016489150b3f
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-01 19:32:57 -0300

    Update merger.py

--- core/merger.py
+++ core/merger.py
@@ -1,1 +1,9 @@
+import pandas as pd
 
+def merge_dados(planilha_df, site_df, url_base, estoque_padrao):
+    if planilha_df.empty:
+        return site_df
+    if site_df.empty:
+        return planilha_df
+
+    return pd.concat([planilha_df, site_df]).drop_duplicates()
