912232fb1d3e0aca2ebf290ad67eb710d8532b57
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-02 03:59:44 -0300

    Update mappers.py

--- core/normalizer/mappers.py
+++ core/normalizer/mappers.py
@@ -1,39 +1,46 @@
 MAPEAMENTO = {
     "codigo": [
+        # máxima prioridade
+        "sku",
+        "sku pai",
+        "sku filho",
         "codigo do produto",
         "código do produto",
         "codigo interno",
         "código interno",
         "codigo sku",
         "código sku",
-        "sku",
+        "referencia",
+        "referência",
+        "ref",
         "codigo",
         "código",
         "cod produto",
         "cod_produto",
-        "referencia",
-        "referência",
-        "ref",
     ],
+
     "gtin": [
+        "gtin/ean",
         "gtin",
         "ean",
-        "gtin/ean",
+        "ean13",
         "codigo de barras",
         "código de barras",
         "barcode",
     ],
+
     "produto": [
         "descricao do produto",
         "descrição do produto",
+        "nome do produto",
+        "produto",
         "descricao",
         "descrição",
-        "nome do produto",
         "nome",
-        "produto",
         "titulo",
         "title",
     ],
+
     "preco": [
         "preco de venda",
         "preço de venda",
@@ -42,6 +49,7 @@ MAPEAMENTO = {
         "valor",
         "price",
     ],
+
     "preco_custo": [
         "preço de custo",
         "preco de custo",
@@ -52,12 +60,16 @@ MAPEAMENTO = {
         "custo",
         "cost",
     ],
+
     "descricao_curta": [
         "descricao curta",
         "descrição curta",
+        "descricao resumida",
+        "descrição resumida",
         "resumo",
         "short description",
     ],
+
     "descricao_complementar": [
         "descricao complementar",
         "descrição complementar",
@@ -68,7 +80,10 @@ MAPEAMENTO = {
         "informações adicionais",
         "informacoes adicionais",
         "detalhes",
+        "observacoes",
+        "observações",
     ],
+
     "imagem": [
         "url imagens externas",
         "url imagem",
@@ -78,7 +93,9 @@ MAPEAMENTO = {
         "image",
         "foto",
         "foto principal",
+        "imagem principal",
     ],
+
     "link": [
         "link externo",
         "url produto",
@@ -88,11 +105,13 @@ MAPEAMENTO = {
         "url",
         "site",
     ],
+
     "marca": [
         "marca",
         "brand",
         "fabricante",
     ],
+
     "estoque": [
         "estoque",
         "quantidade",
@@ -101,42 +120,60 @@ MAPEAMENTO = {
         "saldo",
         "balanco",
         "balanço",
+        "disponivel",
+        "disponível",
     ],
+
     "ncm": [
         "ncm",
+        "classificacao fiscal",
+        "classificação fiscal",
     ],
+
     "origem": [
         "origem",
+        "origem fiscal",
     ],
+
     "peso_liquido": [
         "peso líquido",
         "peso liquido",
         "peso líquido (kg)",
         "peso liquido (kg)",
+        "peso liq",
     ],
+
     "peso_bruto": [
         "peso bruto",
         "peso bruto (kg)",
     ],
+
     "estoque_minimo": [
         "estoque mínimo",
         "estoque minimo",
+        "estoque min",
     ],
+
     "estoque_maximo": [
         "estoque máximo",
         "estoque maximo",
+        "estoque max",
     ],
+
     "unidade": [
         "unidade",
         "un",
         "unidade de medida",
+        "medida",
     ],
+
     "tipo": [
         "tipo",
         "tipo produção",
         "tipo producao",
         "tipo do item",
     ],
+
     "situacao": [
         "situação",
         "situacao",
