659d3c9c425531896010e6536e57c6fae3ed0c95
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-02 00:37:59 -0300

    Update estoque.py

--- core/bling/estoque.py
+++ core/bling/estoque.py
@@ -16,11 +16,11 @@ def preencher_modelo_estoque(modelo, df, depositos):
 
     col_id_produto = encontrar_coluna(["id produto"])
     col_codigo = encontrar_coluna(["código produto", "codigo produto"])
-    col_gtin = encontrar_coluna(["gtin"])
-    col_descricao = encontrar_coluna(["descrição produto", "descricao produto"])
+    col_gtin = encontrar_coluna(["gtin", "codigo de barras", "código de barras", "gtin/ean"])
+    col_descricao = encontrar_coluna(["descrição produto", "descricao produto", "descrição", "descricao"])
     col_deposito = encontrar_coluna(["depósito", "deposito"])
     col_qtd = encontrar_coluna(["balanço", "balanco", "saldo", "estoque"])
-    col_preco = encontrar_coluna(["preço unitário", "preco unitario", "valor"])
+    col_preco = encontrar_coluna(["preço unitário", "preco unitario", "valor", "preço", "preco"])
     col_preco_custo = encontrar_coluna(["preço de custo", "preco de custo"])
     col_observacao = encontrar_coluna(["observação", "observacao"])
     col_data = encontrar_coluna(["data"])
@@ -29,43 +29,43 @@ def preencher_modelo_estoque(modelo, df, depositos):
         for deposito in depositos:
             nova = {col: "" for col in modelo.columns}
 
-            # ID PRODUTO = sempre vazio
+            # ID PRODUTO sempre vazio
             if col_id_produto:
                 nova[col_id_produto] = ""
 
-            # CÓDIGO PRODUTO = SKU / código
+            # Código do produto
             if col_codigo:
                 nova[col_codigo] = row.get("Código", "")
 
-            # GTIN = código de barras
+            # GTIN / código de barras
             if col_gtin:
                 nova[col_gtin] = row.get("GTIN", "")
 
-            # DESCRIÇÃO PRODUTO = título
+            # Descrição do produto = título
             if col_descricao:
                 nova[col_descricao] = row.get("Produto", "")
 
-            # DEPÓSITO
+            # Depósito
             if col_deposito:
                 nova[col_deposito] = deposito
 
-            # SALDO / BALANÇO
+            # Saldo / estoque
             if col_qtd:
                 nova[col_qtd] = row.get("Estoque", 0)
 
-            # PREÇO UNITÁRIO
+            # Preço unitário
             if col_preco:
                 nova[col_preco] = row.get("Preço", "0.01")
 
-            # PREÇO DE CUSTO = vazio
+            # Preço de custo, se existir nos dados
             if col_preco_custo:
-                nova[col_preco_custo] = ""
+                nova[col_preco_custo] = row.get("Preço Custo", "")
 
-            # OBSERVAÇÃO = sempre vazia
+            # Observação sempre vazia
             if col_observacao:
                 nova[col_observacao] = ""
 
-            # DATA = vazia
+            # Data vazia
             if col_data:
                 nova[col_data] = ""
 
