3646c886abe248bf05fc593b76bc5ea4686bce66
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-02 05:14:49 -0300

    Update _init__.py

--- core/bling/_init__.py
+++ core/bling/_init__.py
@@ -1,2 +1,9 @@
-from core.bling.estoque import preencher_modelo_estoque
-from core.bling.cadastro import preencher_modelo_cadastro
+# módulo bling
+
+from .cadastro import preencher_modelo_cadastro
+from .estoque import preencher_modelo_estoque
+
+__all__ = [
+    "preencher_modelo_cadastro",
+    "preencher_modelo_estoque",
+]
