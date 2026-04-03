f2b22497468d15964d76f5a50c39eb01887a8c93
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-03 15:29:17 -0300

    Update __init__.py

--- bling_app_zero/utils/__init__.py
+++ bling_app_zero/utils/__init__.py
@@ -1,8 +1,1 @@
-import pandas as pd
-
-def limpar_planilha(modelo_df):
-    """
-    Mantém apenas as colunas do modelo e remove TODAS as linhas
-    """
-    df_limpo = pd.DataFrame(columns=modelo_df.columns)
-    return df_limpo
+# Subpacote de utilidades.
