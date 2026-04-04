6b68b2b134fe55951c3b4cf88561e22a20e5a982
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-04 16:40:12 -0300

    Update excel.py

--- bling_app_zero/utils/excel.py
+++ bling_app_zero/utils/excel.py
@@ -6,6 +6,6 @@ import pandas as pd
 def df_to_excel_bytes(df: pd.DataFrame, sheet_name: str = "dados") -> bytes:
     output = BytesIO()
     with pd.ExcelWriter(output, engine="openpyxl") as writer:
-        df.to_excel(writer, index=False, sheet_name=sheet_name[:31] or "dados")
+        df.to_excel(writer, index=False, sheet_name=(sheet_name[:31] or "dados"))
     output.seek(0)
     return output.getvalue()
