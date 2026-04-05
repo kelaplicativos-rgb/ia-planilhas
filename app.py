0ec354d38e3d8252a122aff9b500c8f962c86bd4
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-05 20:18:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import traceback
 
 import streamlit as st
 
-APP_VERSION = "1.0.8"
+APP_VERSION = "1.0.9"
 
 from bling_app_zero.ui.state import init_state
 from bling_app_zero.ui import origem_dados as origem_dados_ui
