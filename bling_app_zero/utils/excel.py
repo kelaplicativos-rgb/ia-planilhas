3cf16e7cf8f82f5f3e1e58756022973142628d34
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-07 18:20:04 -0300

    Update excel.py

--- bling_app_zero/utils/excel.py
+++ bling_app_zero/utils/excel.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from typing import Any
+
+import pandas as pd
+
 from .excel_exportacao import (
     df_to_excel_bytes,
     exportar_dataframe_para_excel,
@@ -12,6 +16,14 @@ from .excel_helpers import limpar_gtin_invalido, validar_campos_obrigatorios
 from .excel_leitura import ler_planilha_excel, ler_planilha_segura
 from .excel_logs import baixar_logs_txt, log_debug
 
+
+def safe_df_dados(df: Any) -> bool:
+    try:
+        return isinstance(df, pd.DataFrame) and len(df.columns) > 0
+    except Exception:
+        return False
+
+
 __all__ = [
     "log_debug",
     "baixar_logs_txt",
@@ -19,6 +31,7 @@ __all__ = [
     "validar_campos_obrigatorios",
     "ler_planilha_segura",
     "ler_planilha_excel",
+    "safe_df_dados",
     "df_to_excel_bytes",
     "exportar_df_exato_para_excel_bytes",
     "exportar_dataframe_para_excel",
