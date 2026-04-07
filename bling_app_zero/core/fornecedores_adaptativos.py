6f34f4cf7201b2c6107214d3454c0d2911352e37
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-06 23:41:20 -0300

    Create fornecedores_adaptativos.py

---
+++ bling_app_zero/core/fornecedores_adaptativos.py
@@ -0,0 +1,12 @@
+FORNECEDORES_DB = {
+    "megacentereletronicos.com.br": {
+        "seletores": {
+            "nome": ["h1.product_title"],
+            "preco": [".price"],
+            "descricao": [".description"],
+            "imagem": ["img"],
+        },
+        "tipo": "woocommerce",
+        "confianca": 0.95,
+    }
+}
