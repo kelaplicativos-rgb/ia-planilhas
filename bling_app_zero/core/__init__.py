168a6ae827a781dc01097d2ce522da5975248527
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-10 08:57:46 -0300

    Update __init__.py

--- bling_app_zero/core/__init__.py
+++ bling_app_zero/core/__init__.py
@@ -1,7 +1,11 @@
 from .bling_auth import BlingAuthManager
 from .bling_api import BlingAPIClient
-from .bling_sync import BlingSyncService
 from .bling_token_store import BlingTokenStore
+
+try:
+    from .bling_sync import BlingSyncService
+except Exception:
+    BlingSyncService = None
 
 __all__ = [
     "BlingAuthManager",
