8326e6f912c9d3942f16d2cad8c66ebe793cf7f3
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-16 14:28:13 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+
+import streamlit as st
+
+from bling_app_zero.utils.init_app import init_app
+from bling_app_zero.ui.app_helpers import get_etapa
+from bling_app_zero.ui.origem_dados import render_origem_dados
+from bling_app_zero.ui.origem_precificacao import render_origem_precificacao
+from bling_app_zero.ui.origem_mapeamento import render_origem_mapeamento
+from bling_app_zero.ui.preview_final import render_preview_final
+
+# Inicialização
+init_app()
+
+st.set_page_config(page_title="IA Planilhas → Bling", layout="wide")
+
+etapa = get_etapa()
+
+if etapa == "origem":
+    render_origem_dados()
+
+elif etapa == "precificacao":
+    render_origem_precificacao()
+
+elif etapa == "mapeamento":
+    render_origem_mapeamento()
+
+elif etapa == "final":
+    render_preview_final()
