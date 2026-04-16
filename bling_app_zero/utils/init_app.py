c62bbfc54d7aa26c4f345f19af702d83114d402e
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-16 16:10:48 -0300

    Update init_app.py

--- bling_app_zero/utils/init_app.py
+++ bling_app_zero/utils/init_app.py
@@ -1,14 +1,24 @@
 
 import streamlit as st
 
+
 def init_app():
     defaults = {
         "etapa": "origem",
+        "historico_etapas": ["origem"],
         "df_origem": None,
         "df_modelo": None,
         "tipo_operacao": None,
         "tipo_operacao_bling": None,
         "deposito_nome": "",
+        "origem_upload_nome": "",
+        "origem_upload_bytes": None,
+        "origem_upload_tipo": "",
+        "origem_upload_ext": "",
+        "modelo_upload_nome": "",
+        "modelo_upload_bytes": None,
+        "modelo_upload_tipo": "",
+        "modelo_upload_ext": "",
     }
 
     for k, v in defaults.items():
