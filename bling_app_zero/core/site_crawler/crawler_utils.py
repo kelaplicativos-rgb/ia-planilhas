b6361f1a537054230030e1b37994b8c232aa1154
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-27 18:41:24 -0300

    Update crawler_utils.py

--- bling_app_zero/core/site_crawler/crawler_utils.py
+++ bling_app_zero/core/site_crawler/crawler_utils.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+
+def log(msg):
+    print(msg)
+
+    if "logs" not in st.session_state:
+        st.session_state["logs"] = []
+
+    st.session_state["logs"].append(msg)
