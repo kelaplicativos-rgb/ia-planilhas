86181dec5af3f0188c9c0d3f07c5db6a826c676e
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-27 18:41:01 -0300

    Update http_client.py

--- bling_app_zero/core/site_crawler/http_client.py
+++ bling_app_zero/core/site_crawler/http_client.py
@@ -1,1 +1,14 @@
+import requests
 
+
+def build_session(auth_context=None):
+    session = requests.Session()
+
+    session.headers.update({
+        "User-Agent": "Mozilla/5.0",
+        "Accept": "*/*",
+    })
+
+    session.verify = False
+
+    return session
