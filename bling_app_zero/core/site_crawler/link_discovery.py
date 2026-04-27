19d7d90b8f72b87c618c5fe983b1ccad509fe2e2
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-27 18:42:52 -0300

    Update link_discovery.py

--- bling_app_zero/core/site_crawler/link_discovery.py
+++ bling_app_zero/core/site_crawler/link_discovery.py
@@ -1,1 +1,24 @@
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
+
+def extract_product_links(html, base):
+    soup = BeautifulSoup(html, "lxml")
+
+    links = []
+
+    for a in soup.find_all("a", href=True):
+        href = urljoin(base, a["href"])
+
+        if "/produto" in href or "/p/" in href:
+            links.append(href)
+
+    return links
+
+
+def extract_category_links(html, base):
+    return []
+
+
+def extract_pagination_links(html, base):
+    return []
