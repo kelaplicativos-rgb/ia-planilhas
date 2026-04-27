ee27a437814389ecae0f94d69599693455f1c5e9
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-27 18:43:12 -0300

    Update sitemap_engine.py

--- bling_app_zero/core/site_crawler/sitemap_engine.py
+++ bling_app_zero/core/site_crawler/sitemap_engine.py
@@ -1,1 +1,16 @@
+def discover_links_from_sitemap(session, base_url):
+    urls = []
 
+    try:
+        sitemap = base_url.rstrip("/") + "/sitemap.xml"
+        r = session.get(sitemap, timeout=20, verify=False)
+
+        if r.status_code == 200:
+            for line in r.text.split():
+                if "<loc>" in line:
+                    urls.append(line.replace("<loc>", "").replace("</loc>", ""))
+
+    except Exception:
+        pass
+
+    return urls
