4a0f0c987e65cf658f439c13385abbdcaa99994b
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-29 18:11:48 -0300

    Update __init__.py

--- bling_app_zero/core/instant_scraper/__init__.py
+++ bling_app_zero/core/instant_scraper/__init__.py
@@ -1,1 +1,17 @@
+# bling_app_zero/core/instant_scraper/__init__.py
 
+from __future__ import annotations
+
+try:
+    from .runner import buscar_dataframe, buscar_produtos, run_scraper
+except Exception:
+    run_scraper = None
+    buscar_dataframe = None
+    buscar_produtos = None
+
+
+__all__ = [
+    "run_scraper",
+    "buscar_dataframe",
+    "buscar_produtos",
+]
