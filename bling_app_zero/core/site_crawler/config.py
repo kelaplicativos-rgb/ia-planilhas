b5e29f37128643b96a5102bd1f82bae4aabf9135
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-30 15:55:33 -0300

    BLINGMODULAR GLOBAL: adiciona config do site crawler

---
+++ bling_app_zero/core/site_crawler/config.py
@@ -0,0 +1,47 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+
+BLOCKED_URL_PARTS = [
+    "carrinho",
+    "cart",
+    "checkout",
+    "login",
+    "minha-conta",
+    "account",
+    "wishlist",
+    "favoritos",
+    "javascript:",
+    "mailto:",
+    "tel:",
+    "politica",
+    "privacidade",
+    "termos",
+]
+
+
+PRODUCT_HINTS = [
+    "/produto",
+    "/product",
+    "/p/",
+    "produto/",
+]
+
+
+CATEGORY_HINTS = [
+    "/categoria",
+    "/category",
+    "/departamento",
+    "/colecao",
+    "/collections",
+]
+
+
+@dataclass
+class CrawlConfig:
+    max_urls: int = 250
+    max_products: int = 300
+    max_depth: int = 2
+    timeout: int = 15
+    sleep_seconds: float = 0.15
