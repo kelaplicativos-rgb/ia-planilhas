5507d8f56c9c41487e59d65aaf3ead4160c5c59d
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-30 12:24:06 -0300

    BLINGNEXT CONNECT ULTRA: corrige export do instant_scraper

--- bling_app_zero/core/instant_scraper/__init__.py
+++ bling_app_zero/core/instant_scraper/__init__.py
@@ -2,12 +2,31 @@
 
 from __future__ import annotations
 
+import pandas as pd
+
 try:
-    from .runner import buscar_dataframe, buscar_produtos, run_scraper
+    from .runner import run_scraper as _run_scraper
 except Exception:
-    run_scraper = None
-    buscar_dataframe = None
-    buscar_produtos = None
+    _run_scraper = None
+
+
+def run_scraper(url: str, *args, **kwargs) -> pd.DataFrame:
+    if _run_scraper is None:
+        return pd.DataFrame()
+    try:
+        return _run_scraper(url, *args, **kwargs)
+    except TypeError:
+        return _run_scraper(url)
+    except Exception:
+        return pd.DataFrame()
+
+
+def buscar_dataframe(url: str, *args, **kwargs) -> pd.DataFrame:
+    return run_scraper(url, *args, **kwargs)
+
+
+def buscar_produtos(url: str, *args, **kwargs) -> pd.DataFrame:
+    return run_scraper(url, *args, **kwargs)
 
 
 __all__ = [
