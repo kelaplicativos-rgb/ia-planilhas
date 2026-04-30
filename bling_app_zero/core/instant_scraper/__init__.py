828acf2e9490a5c295ae6c98a1ab7e3ed8e0bcc5
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-04-30 20:29:23 -0300

    Expoe Flash Instant Scraper no pacote

--- bling_app_zero/core/instant_scraper/__init__.py
+++ bling_app_zero/core/instant_scraper/__init__.py
@@ -9,6 +9,11 @@ try:
 except Exception:
     _run_scraper = None
 
+try:
+    from .flash_scraper import run_flash_scraper
+except Exception:
+    run_flash_scraper = None
+
 
 def run_scraper(url: str, *args, **kwargs) -> pd.DataFrame:
     if _run_scraper is None:
@@ -17,6 +22,17 @@ def run_scraper(url: str, *args, **kwargs) -> pd.DataFrame:
         return _run_scraper(url, *args, **kwargs)
     except TypeError:
         return _run_scraper(url)
+    except Exception:
+        return pd.DataFrame()
+
+
+def run_flash(url: str, *args, **kwargs) -> pd.DataFrame:
+    if run_flash_scraper is None:
+        return pd.DataFrame()
+    try:
+        return run_flash_scraper(url, *args, **kwargs)
+    except TypeError:
+        return run_flash_scraper(url)
     except Exception:
         return pd.DataFrame()
 
@@ -31,6 +47,8 @@ def buscar_produtos(url: str, *args, **kwargs) -> pd.DataFrame:
 
 __all__ = [
     "run_scraper",
+    "run_flash",
+    "run_flash_scraper",
     "buscar_dataframe",
     "buscar_produtos",
 ]
