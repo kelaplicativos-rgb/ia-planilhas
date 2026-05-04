1c9a969dbaeff38236faeb1f6a62947a17279a46
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 18:57:51 -0300

    Remove rota antiga de precificacao do wizard compacto

--- bling_app_zero/ui/step_router.py
+++ bling_app_zero/ui/step_router.py
@@ -7,7 +7,6 @@ from bling_app_zero.ui.mod_kernel import call
 
 ROUTES = {
     "origem": ("bling_app_zero.ui.origem", "render_origem_dados"),
-    "precificacao": ("bling_app_zero.ui.precificacao", "render_origem_precificacao"),
     "mapeamento": ("bling_app_zero.ui.mapeamento", "render_origem_mapeamento"),
     "preview_final": ("bling_app_zero.ui.preview", "render_preview_final"),
 }
