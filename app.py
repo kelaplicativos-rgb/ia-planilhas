1c70a1ecd275d0feccececd043c445758df4d2cd
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 19:01:52 -0300

    Liga guarda de compatibilidade do fluxo compacto

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bling_app_zero.utils.init_app import init_app
 from bling_app_zero.ui.app_core_state import init_state
 from bling_app_zero.ui.app_core_layout import render_header, render_nav
 from bling_app_zero.ui.app_core_flow import set_etapa_segura, sincronizar_fluxo_inicial
+from bling_app_zero.ui.compact_flow_guard import normalize_compact_flow_state
 from bling_app_zero.ui.health_panel import render_health_panel
 from bling_app_zero.ui.step_router import render_step
 from bling_app_zero.ui.workspace_panel import render_workspace_panel
@@ -26,6 +27,7 @@ def main():
     init_app()
     init_state()
     sincronizar_fluxo_inicial()
+    normalize_compact_flow_state()
 
     cfg = get_enterprise_config()
 
