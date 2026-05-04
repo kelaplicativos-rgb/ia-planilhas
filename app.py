7c23f24a5b6a98fcf68609c8a35d28236e41178a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 11:44:47 -0300

    BLINGMODULAR app usa step router lazy

--- app.py
+++ app.py
@@ -4,22 +4,7 @@ from bling_app_zero.utils.init_app import init_app
 from bling_app_zero.ui.app_core_state import init_state
 from bling_app_zero.ui.app_core_layout import render_header, render_nav
 from bling_app_zero.ui.app_core_flow import set_etapa_segura, sincronizar_fluxo_inicial
-
-from bling_app_zero.ui.origem import render_origem_dados
-from bling_app_zero.ui.precificacao import render_origem_precificacao
-from bling_app_zero.ui.mapeamento import render_origem_mapeamento
-from bling_app_zero.ui.preview import render_preview_final
-
-
-def render_etapa(etapa):
-    if etapa == "origem":
-        render_origem_dados()
-    elif etapa == "precificacao":
-        render_origem_precificacao()
-    elif etapa == "mapeamento":
-        render_origem_mapeamento()
-    elif etapa == "preview_final":
-        render_preview_final()
+from bling_app_zero.ui.step_router import render_step
 
 
 def trocar_etapa(etapa):
@@ -35,7 +20,7 @@ def main():
     etapa_max = st.session_state.get("wizard_etapa_maxima", "origem")
     render_header()
     render_nav(etapa, etapa_max, trocar_etapa)
-    render_etapa(etapa)
+    render_step(etapa)
 
 
 if __name__ == "__main__":
