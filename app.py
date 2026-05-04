055fca5bbb378f8cbbbccbb2bd4aef723ebc48d2
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 11:47:09 -0300

    BLINGMODULAR carrega hooks no boot

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bling_app_zero.utils.init_app import init_app
 from bling_app_zero.ui.app_core_state import init_state
 from bling_app_zero.ui.app_core_layout import render_header, render_nav
 from bling_app_zero.ui.app_core_flow import set_etapa_segura, sincronizar_fluxo_inicial
+from bling_app_zero.ui.hook_registry import load_hooks
 from bling_app_zero.ui.step_router import render_step
 
 
@@ -15,6 +16,7 @@ def main():
     st.set_page_config(page_title="IA Planilhas Bling", page_icon="🚀", layout="wide")
     init_app()
     init_state()
+    load_hooks()
     sincronizar_fluxo_inicial()
     etapa = st.session_state.get("wizard_etapa_atual", "origem")
     etapa_max = st.session_state.get("wizard_etapa_maxima", "origem")
