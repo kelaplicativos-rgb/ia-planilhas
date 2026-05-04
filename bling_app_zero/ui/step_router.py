266cafdb80717bb62743bb9c5c6d7e14b3304697
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 15:37:25 -0300

    BLINGHUB registra rota hub

--- bling_app_zero/ui/step_router.py
+++ bling_app_zero/ui/step_router.py
@@ -7,6 +7,7 @@ from bling_app_zero.ui.mod_kernel import call
 
 ROUTES = {
     "origem": ("bling_app_zero.ui.origem", "render_origem_dados"),
+    "hub": ("bling_app_zero.ui.hub_dashboard", "render_hub_dashboard"),
     "precificacao": ("bling_app_zero.ui.precificacao", "render_origem_precificacao"),
     "mapeamento": ("bling_app_zero.ui.mapeamento", "render_origem_mapeamento"),
     "preview_final": ("bling_app_zero.ui.preview", "render_preview_final"),
