2d4bfa4dbc34f6c61e567823913b3c3bdf53e31c
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 11:52:29 -0300

    BLINGSCAN exibe painel de diagnostico

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bling_app_zero.utils.init_app import init_app
 from bling_app_zero.ui.app_core_state import init_state
 from bling_app_zero.ui.app_core_layout import render_header, render_nav
 from bling_app_zero.ui.app_core_flow import set_etapa_segura, sincronizar_fluxo_inicial
+from bling_app_zero.ui.health_panel import render_health_panel
 from bling_app_zero.ui.hook_registry import load_hooks
 from bling_app_zero.ui.step_router import render_step
 
@@ -21,6 +22,7 @@ def main():
     etapa = st.session_state.get("wizard_etapa_atual", "origem")
     etapa_max = st.session_state.get("wizard_etapa_maxima", "origem")
     render_header()
+    render_health_panel()
     render_nav(etapa, etapa_max, trocar_etapa)
     render_step(etapa)
 
