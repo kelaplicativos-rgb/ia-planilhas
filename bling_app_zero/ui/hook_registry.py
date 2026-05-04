2bdf02979b41101779c11fd6da9dae88e92f7a90
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 12:13:42 -0300

    fix hook registry

--- bling_app_zero/ui/hook_registry.py
+++ bling_app_zero/ui/hook_registry.py
@@ -6,5 +6,5 @@ from bling_app_zero.ui.hook_bus import register
 def load_hooks() -> None:
     register(
         "preview.before_download",
-        "bling_app_zero.ui.preview_final_estoque_inteligente:aplicar_estoque_inteligente_final",
+        "bling_app_zero.ui.plugins.estoque_final:before_download",
     )
