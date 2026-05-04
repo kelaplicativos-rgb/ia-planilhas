9313fed760767e0faf52bc494b672dc2f5638830
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 12:15:29 -0300

    BLINGSCAN remove hooks soltos do boot

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from bling_app_zero.ui.app_core_state import init_state
 from bling_app_zero.ui.app_core_layout import render_header, render_nav
 from bling_app_zero.ui.app_core_flow import set_etapa_segura, sincronizar_fluxo_inicial
 from bling_app_zero.ui.health_panel import render_health_panel
-from bling_app_zero.ui.hook_registry import load_hooks
 from bling_app_zero.ui.step_router import render_step
 
 
@@ -17,7 +16,6 @@ def main():
     st.set_page_config(page_title="IA Planilhas Bling", page_icon="🚀", layout="wide")
     init_app()
     init_state()
-    load_hooks()
     sincronizar_fluxo_inicial()
     etapa = st.session_state.get("wizard_etapa_atual", "origem")
     etapa_max = st.session_state.get("wizard_etapa_maxima", "origem")
