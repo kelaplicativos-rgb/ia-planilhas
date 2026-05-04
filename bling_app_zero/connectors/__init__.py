473466be6e0819d3391a753ee5755685aee4f459
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 15:36:09 -0300

    BLINGHUB cria pacote de conectores

---
+++ bling_app_zero/connectors/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+"""Conectores de entrada/saída do BLINGHUB.
+
+A pasta separa integrações por origem/destino sem misturar regra de negócio com UI.
+"""
+
+__all__ = []
