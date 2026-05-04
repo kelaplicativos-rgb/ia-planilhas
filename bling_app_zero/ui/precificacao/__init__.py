22e01a58dddfdbf7ce1d468a4047c4eb50edcfd9
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-04 11:39:31 -0300

    BLINGMODULAR usa page precificacao

--- bling_app_zero/ui/precificacao/__init__.py
+++ bling_app_zero/ui/precificacao/__init__.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-from bling_app_zero.ui.origem_precificacao import render_origem_precificacao
+from bling_app_zero.ui.precificacao.page import render_origem_precificacao
 
 __all__ = ["render_origem_precificacao"]
