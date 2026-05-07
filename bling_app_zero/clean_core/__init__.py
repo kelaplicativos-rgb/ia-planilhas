de6a7b49c117355609a5bed3d5e41c5ed743e86a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-07 13:52:52 -0300

    clean-core: cria pacote base validado

---
+++ bling_app_zero/clean_core/__init__.py
@@ -0,0 +1,11 @@
+"""Núcleo limpo e validado do IA Planilhas Bling.
+
+Regra desta fase:
+- sem monkey patch;
+- sem crawler automático;
+- sem misturar cadastro com estoque;
+- modelo anexado define exatamente as colunas finais;
+- CSV final sempre separado por ';' e UTF-8-SIG.
+"""
+
+CLEAN_CORE_VERSION = "0.1.0"
