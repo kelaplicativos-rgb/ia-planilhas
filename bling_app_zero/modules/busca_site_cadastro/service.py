d63bb712a25e08bb4fa2ea0210c2a6847f00e755
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-07 16:53:57 -0300

    BLINGMODULAR: criar busca_site_cadastro

---
+++ bling_app_zero/modules/busca_site_cadastro/service.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+
+class BuscaSiteCadastroService:
+    """
+    Busca completa para cadastro de produtos.
+
+    Pode capturar:
+    - descrição
+    - imagens
+    - GTIN
+    - categoria
+    - marca
+    - atributos
+    - descrição complementar
+    """
+
+    FLOW_NAME = "busca_site_cadastro"
