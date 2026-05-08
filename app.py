85251264ac60e2bd49edace2eada85b70f2551f8
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-08 16:17:17 -0300

    Atualiza versao frase completa

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bling_app_zero.core.debug import add_debug, render_debug_panel
 from bling_app_zero.ui.home import render_home
 
 
-APP_VERSION = '3.3.0-FRASE-PERSUASIVA'
+APP_VERSION = '3.3.1-FRASE-COMPLETA'
 
 
 def _register_critical_error(exc: Exception) -> str:
