e4a4353aa7589956a499d5603628bd517cfc5967
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-08 16:22:50 -0300

    Atualiza versao origem site para planilha

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bling_app_zero.core.debug import add_debug, render_debug_panel
 from bling_app_zero.ui.home import render_home
 
 
-APP_VERSION = '3.3.1-FRASE-COMPLETA'
+APP_VERSION = '3.3.2-SITE-VIRA-PLANILHA'
 
 
 def _register_critical_error(exc: Exception) -> str:
