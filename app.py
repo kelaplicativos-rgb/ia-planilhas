50a0ddf3ae0704fc6ef4a2079abe038316cde31c
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-08 17:33:42 -0300

    Atualiza versao predefinicoes finais

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bling_app_zero.core.debug import add_debug, render_debug_panel
 from bling_app_zero.ui.home import render_home
 
 
-APP_VERSION = '3.3.6-MEDIDAS-PADRAO'
+APP_VERSION = '3.3.7-PREDEFINICOES-FINAIS'
 
 
 def _register_critical_error(exc: Exception) -> str:
