4b7ec18cf342e1cb486579151dee568dfaea6ea2
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-08 12:32:54 -0300

    BLINGFIX: atualizar versão BLINGFLOW

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from bling_app_zero.core.debug import add_debug, render_debug_panel
 from bling_app_zero.ui.home import render_home
 
 
-APP_VERSION = '3.0.2-BLINGFIX-DEBUG'
+APP_VERSION = '3.0.3-BLINGFLOW-SIMULATION'
 
 
 def _register_critical_error(exc: Exception) -> str:
