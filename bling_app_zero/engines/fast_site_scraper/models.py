7df083499a857a728730db531839a11bec63278b
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-08 17:48:42 -0300

    Cria modelos do fast scraper

---
+++ bling_app_zero/engines/fast_site_scraper/models.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+
+@dataclass(frozen=True)
+class FastProductPage:
+    url: str
+    html: str
+    text: str
+    jsonld_products: list[dict] = field(default_factory=list)
+
+
+@dataclass(frozen=True)
+class FastProductData:
+    url: str = ''
+    codigo: str = ''
+    gtin: str = ''
+    descricao: str = ''
+    preco: str = ''
+    estoque: str = ''
+    imagem: str = ''
+    marca: str = ''
+    categoria: str = ''
