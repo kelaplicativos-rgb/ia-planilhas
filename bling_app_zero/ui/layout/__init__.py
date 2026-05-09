7e2ba1c1bda1097f182a412e200d6116d1f82140
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-09 15:17:37 -0300

    BLINGMODULAR LAYOUT: criar pacote oficial de layout

---
+++ bling_app_zero/ui/layout/__init__.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.layout.components import (
+    close_home_start_card,
+    render_compact_hero,
+    render_compact_note,
+    render_home_pricing_card,
+    render_home_start_card,
+    render_step_title,
+)
+from bling_app_zero.ui.layout.mapping import inject_mapping_css, render_mapping_preview, render_mapping_title
+from bling_app_zero.ui.layout.theme import inject_app_layout, inject_clean_home_css, inject_unified_light_layout
+
+__all__ = [
+    'inject_app_layout',
+    'inject_clean_home_css',
+    'inject_unified_light_layout',
+    'inject_mapping_css',
+    'render_mapping_title',
+    'render_mapping_preview',
+    'render_compact_hero',
+    'render_home_start_card',
+    'render_home_pricing_card',
+    'close_home_start_card',
+    'render_step_title',
+    'render_compact_note',
+]
