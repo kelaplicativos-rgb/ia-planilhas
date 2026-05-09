5e21540c3dcf8a37a98b18ed96cb5b019ccde2a8
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-09 15:18:27 -0300

    BLINGMODULAR LAYOUT: criar componentes centralizados

---
+++ bling_app_zero/ui/layout/components.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.clean_layout import (
+    close_home_start_card,
+    render_compact_hero,
+    render_compact_note,
+    render_home_pricing_card,
+    render_home_start_card,
+    render_step_title,
+)
+
+__all__ = [
+    'render_compact_hero',
+    'render_home_start_card',
+    'render_home_pricing_card',
+    'close_home_start_card',
+    'render_step_title',
+    'render_compact_note',
+]
