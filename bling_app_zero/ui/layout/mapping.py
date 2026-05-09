97689e46b5ac1fc9dd2dea1b8e57191622e64c03
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-09 15:18:39 -0300

    BLINGMODULAR LAYOUT: criar layout de mapeamento centralizado

---
+++ bling_app_zero/ui/layout/mapping.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.mapping_layout import inject_mapping_css, render_mapping_preview, render_mapping_title
+
+__all__ = [
+    'inject_mapping_css',
+    'render_mapping_title',
+    'render_mapping_preview',
+]
