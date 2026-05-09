53735b621be1fb9a496e1aaaa49c63e4de4f8143
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-09 15:20:20 -0300

    BLINGMODULAR LAYOUT: modelos usam pacote oficial

--- bling_app_zero/ui/home_models.py
+++ bling_app_zero/ui/home_models.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import pandas as pd
 import streamlit as st
 
-from bling_app_zero.ui.clean_layout import render_compact_note
+from bling_app_zero.ui.layout import render_compact_note
 from bling_app_zero.ui.model_upload import render_model_upload_box
 
 HOME_CADASTRO_MODEL_KEY = 'home_modelo_cadastro_df'
