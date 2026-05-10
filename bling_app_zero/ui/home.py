f338829d1fe83b65ff989d5c3c0ef69533fefb71
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-09 23:27:54 -0300

    BLINGNEXT: acoplar guardiao de estado na home

--- bling_app_zero/ui/home.py
+++ bling_app_zero/ui/home.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-import streamlit as st
-
 from bling_app_zero.ui.home_wizard import render_home_wizard
 from bling_app_zero.ui.layout import inject_app_layout, render_compact_hero
+from bling_app_zero.ui.wizard_state_guard import run_wizard_state_guard
 
 
 def render_home() -> None:
+    run_wizard_state_guard()
     inject_app_layout()
     render_compact_hero()
     render_home_wizard()
