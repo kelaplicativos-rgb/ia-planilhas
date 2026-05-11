7a10f0cf20f31adbd52811a345b976d5505270e6
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-11 20:40:49 -0300

    Bump app version after BLINGCLEAN blocked card

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from bling_app_zero.ui.home import render_home
 from bling_app_zero.ui.sidebar_tools import render_sidebar_tools
 
 
-APP_VERSION = '3.5.19-BLINGCLEAN-MAPPING-PAGINADO'
+APP_VERSION = '3.5.20-BLINGCLEAN-BLOCKED-CARD'
 
 
 def _inject_streamlit_toolbar_fix() -> None:
