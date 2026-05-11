bd65e484972b828a33a24e09437dd43c90704430
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-11 12:30:55 -0300

    BLINGFIX atualiza versao estoque site independente

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from bling_app_zero.ui.home import render_home
 from bling_app_zero.ui.sidebar_tools import render_sidebar_tools
 
 
-APP_VERSION = '3.5.16-BLINGFIX-NAMEERROR-PRECIFICACAO'
+APP_VERSION = '3.5.17-BLINGFIX-ESTOQUE-SITE-INDEPENDENTE'
 
 
 def _inject_streamlit_toolbar_fix() -> None:
