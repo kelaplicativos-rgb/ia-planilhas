653e9b372922659034d99a50feddec7b0ee32969
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-11 11:32:08 -0300

    BLINGFIX version: corrigir NameError precificação

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from bling_app_zero.ui.home import render_home
 from bling_app_zero.ui.sidebar_tools import render_sidebar_tools
 
 
-APP_VERSION = '3.5.15-BLINGFIX-SIDEBAR-CLOSE-BUTTON'
+APP_VERSION = '3.5.16-BLINGFIX-NAMEERROR-PRECIFICACAO'
 
 
 def _inject_streamlit_toolbar_fix() -> None:
