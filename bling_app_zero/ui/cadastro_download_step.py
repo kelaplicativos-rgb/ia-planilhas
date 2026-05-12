c4b36d69f8f39d227633691c1080e2b910342a78
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 17:07:34 -0300

    BLINGFIX mostra aviso do filtro mestre no download

--- bling_app_zero/ui/cadastro_download_step.py
+++ bling_app_zero/ui/cadastro_download_step.py
@@ -6,6 +6,7 @@ from bling_app_zero.ui.cadastro_wizard_state import (
     BLING_IMPORTADOR_PRODUTOS_URL,
     enforce_cadastro_model_columns,
     render_row_count_blocker,
+    render_supplier_price_master_notice,
     valid_df,
 )
 from bling_app_zero.ui.home_shared import download_final
@@ -19,6 +20,8 @@ def render_cadastro_download_step() -> None:
     if not valid_df(df_final):
         st.warning('Ainda não há CSV final de cadastro. Volte para o preview.')
         return
+
+    render_supplier_price_master_notice(df_final)
 
     if render_row_count_blocker(df_final):
         return
