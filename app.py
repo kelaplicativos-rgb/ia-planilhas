782b7d7f040caea88b896e18ad48ac4897257b9e
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 00:32:18 -0300

    BLINGFIX: restaura valores manuais antes do mapeamento

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from bling_app_zero.core import APP_VERSION, PAGE_CONFIG, register_critical_error
 from bling_app_zero.core.audit import add_audit_event, audit_session_state_changes
 from bling_app_zero.core.debug import add_debug
+from bling_app_zero.core.mapping_widget_state import restore_mapping_widget_state_from_snapshot
 from bling_app_zero.ui.home import render_home
 from bling_app_zero.ui.layout import inject_streamlit_toolbar_fix
 from bling_app_zero.ui.sidebar_tools import render_sidebar_tools
@@ -14,6 +15,7 @@ def main() -> None:
     st.set_page_config(**PAGE_CONFIG)
     inject_streamlit_toolbar_fix()
 
+    restore_mapping_widget_state_from_snapshot()
     audit_session_state_changes(stage='app_start')
     add_debug(f'Aplicacao iniciada | versao {APP_VERSION}', origin='APP')
     add_audit_event('app_started', area='APP', details={'version': APP_VERSION})
