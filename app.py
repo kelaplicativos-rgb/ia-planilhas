b7852618e7a887145ba70079e968617be18b31d7
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-11 21:28:29 -0300

    Bump app version after pricing flow cleanup

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from bling_app_zero.ui.home import render_home
 from bling_app_zero.ui.sidebar_tools import render_sidebar_tools
 
 
-APP_VERSION = '3.5.20-BLINGCLEAN-BLOCKED-CARD'
+APP_VERSION = '3.5.21-BLINGCLEAN-PRICING-ESTOQUE'
 
 
 def _inject_streamlit_toolbar_fix() -> None:
