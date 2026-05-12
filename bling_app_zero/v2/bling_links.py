01717863f59db279d9c4214b64a91120fa8d76a4
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 15:26:53 -0300

    BLINGV2: adicionar links oficiais de importacao

---
+++ bling_app_zero/v2/bling_links.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+BLING_MULTISTORE_PRICE_IMPORT_URL = 'https://www.bling.com.br/importador.precos.produtos.multiloja.php'
+
+__all__ = ['BLING_MULTISTORE_PRICE_IMPORT_URL']
