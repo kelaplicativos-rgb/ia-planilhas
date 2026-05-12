080ef3d63e11a311a3b2c127333efe854d0f3b81
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 15:17:33 -0300

    BLINGV2: conectar roteador de operacoes na home

--- bling_app_zero/ui/home.py
+++ bling_app_zero/ui/home.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from bling_app_zero.ui.home_wizard import render_home_wizard
+from bling_app_zero.ui.home_router import render_home_router
 from bling_app_zero.ui.layout import inject_app_layout, render_compact_hero
 from bling_app_zero.ui.wizard_state_guard import run_wizard_state_guard
 
@@ -9,7 +9,7 @@ def render_home() -> None:
     run_wizard_state_guard()
     inject_app_layout()
     render_compact_hero()
-    render_home_wizard()
+    render_home_router()
 
 
 __all__ = ['render_home']
