55b662abf852deef50450e27429d141e96920ca9
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 14:50:11 -0300

    BLINGV2: criar nucleo modular limpo independente

---
+++ bling_app_zero/v2/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from bling_app_zero.v2.contracts import (
+    BlingOperation,
+    BlingStage,
+    ModuleResult,
+    ModuleSpec,
+    SheetAdapterSpec,
+    StoreProfile,
+    TablePayload,
+)
+from bling_app_zero.v2.module_kernel import ModuleKernel, build_default_kernel
+
+__all__ = [
+    'BlingOperation',
+    'BlingStage',
+    'ModuleKernel',
+    'ModuleResult',
+    'ModuleSpec',
+    'SheetAdapterSpec',
+    'StoreProfile',
+    'TablePayload',
+    'build_default_kernel',
+]
