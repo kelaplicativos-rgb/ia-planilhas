4781458d33adf46cd8f83eb52ad8063aa6ace4de
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 09:43:33 -0300

    BLINGFIX: usa fluxos modulares no mapeamento compartilhado

--- bling_app_zero/ui/shared_mapping.py
+++ bling_app_zero/ui/shared_mapping.py
@@ -5,6 +5,8 @@ import streamlit as st
 
 from bling_app_zero.engines.cadastro_engine import default_model as cadastro_default_model
 from bling_app_zero.flows.estoque_contract import default_model as estoque_default_model
+from bling_app_zero.ui.mapping_cadastro_flow import render_manual_mapping
+from bling_app_zero.ui.mapping_estoque_flow import render_manual_stock_mapping
 from bling_app_zero.ui.mapping_review_panel import render_mapping_review_panel
 
 
@@ -17,8 +19,6 @@ def _model_columns(df_modelo: pd.DataFrame | None, operation: str) -> list[str]:
 
 
 def render_shared_cadastro_mapping(df_source: pd.DataFrame, df_modelo: pd.DataFrame | None) -> None:
-    from bling_app_zero.ui.cadastro_mapping import render_manual_mapping
-
     render_manual_mapping(df_source, df_modelo)
     render_mapping_review_panel(
         operation='cadastro',
@@ -34,8 +34,6 @@ def render_shared_stock_mapping(
     df_modelo_estoque: pd.DataFrame | None,
     deposito: str,
 ) -> None:
-    from bling_app_zero.ui.cadastro_mapping import render_manual_stock_mapping
-
     render_manual_stock_mapping(df_source, df_modelo_estoque, deposito)
     render_mapping_review_panel(
         operation='estoque',
