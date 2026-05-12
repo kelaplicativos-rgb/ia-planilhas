bdaa211d6dc9cef7b989229c6cd4df0c4cfcc452
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 14:33:44 -0300

    BLINGMODULE: criar pacote oficial de recursos plugaveis

---
+++ bling_app_zero/features/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+from bling_app_zero.features.registry import FEATURE_REGISTRY, FeatureDefinition, get_feature, list_features
+
+__all__ = [
+    'FEATURE_REGISTRY',
+    'FeatureDefinition',
+    'get_feature',
+    'list_features',
+]
