14d595627f6909a3b0d51481c3d03ac7f4362bd8
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-12 15:10:39 -0300

    BLINGV2: criar pacote preco multiloja

---
+++ bling_app_zero/v2/price_multistore/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from bling_app_zero.v2.price_multistore.detector import detect_multistore_model
+from bling_app_zero.v2.price_multistore.flow import run_multistore_price_flow
+
+__all__ = ['detect_multistore_model', 'run_multistore_price_flow']
