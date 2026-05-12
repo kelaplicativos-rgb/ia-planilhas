c94de08b817b36455a26723a831d594a630649f6
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-11 23:37:47 -0300

    BLINGCLEAN: exporta config e erros pelo core

--- bling_app_zero/core/__init__.py
+++ bling_app_zero/core/__init__.py
@@ -1,3 +1,8 @@
-"""Core compartilhado: normalização, validação, GTIN, exportação e leitura."""
+"""Core compartilhado: configuração, erros, normalização, validação, GTIN, exportação e leitura."""
 
-__all__ = []
+from __future__ import annotations
+
+from bling_app_zero.core.app_config import APP_VERSION, PAGE_CONFIG
+from bling_app_zero.core.app_errors import register_critical_error
+
+__all__ = ['APP_VERSION', 'PAGE_CONFIG', 'register_critical_error']
