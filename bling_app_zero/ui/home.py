72c513a837593e2978a53f41cd73b231e15f077f
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-13 15:57:27 -0300

    BLINGFIX ativar preservacao global de scroll

--- bling_app_zero/ui/home.py
+++ bling_app_zero/ui/home.py
@@ -2,12 +2,14 @@ from __future__ import annotations
 
 from bling_app_zero.ui.home_router import render_home_router
 from bling_app_zero.ui.layout import inject_app_layout, render_compact_hero
+from bling_app_zero.ui.scroll_position import inject_scroll_position_keeper
 from bling_app_zero.ui.wizard_state_guard import run_wizard_state_guard
 
 
 def render_home() -> None:
     run_wizard_state_guard()
     inject_app_layout()
+    inject_scroll_position_keeper()
     render_compact_hero()
     render_home_router()
 
