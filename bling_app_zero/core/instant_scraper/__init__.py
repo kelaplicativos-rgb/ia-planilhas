a8c9fcc32f72fe02feffe720cd66ac327d9f7251
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-13 17:46:25 -0300

    BLINGFIX exporta motor navegador instant scraper

--- bling_app_zero/core/instant_scraper/__init__.py
+++ bling_app_zero/core/instant_scraper/__init__.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
+from .browser_engine import BrowserScraperConfig, BrowserScraperResult, run_browser_scraper
 from .engine import FlashScraperEngine, FlashScraperResult, run_flash_scraper
 from .smart_fields import FieldRequest, build_field_requests
 
 __all__ = [
+    "BrowserScraperConfig",
+    "BrowserScraperResult",
     "FlashScraperEngine",
     "FlashScraperResult",
     "FieldRequest",
     "build_field_requests",
+    "run_browser_scraper",
     "run_flash_scraper",
 ]
