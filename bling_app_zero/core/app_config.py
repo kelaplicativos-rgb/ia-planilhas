bc7a731117eccb7be5d98903c2c35db6b0d74681
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-14 11:36:34 -0300

    Update app version for internal BLINGLOGS

--- bling_app_zero/core/app_config.py
+++ bling_app_zero/core/app_config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-APP_VERSION = '3.5.38-BLINGLOGS-DESCRICAO-SITE'
+APP_VERSION = '3.5.39-BLINGLOGS-INTERNAL'
 
 PAGE_CONFIG = {
     'page_title': 'IA Planilhas → Bling',
