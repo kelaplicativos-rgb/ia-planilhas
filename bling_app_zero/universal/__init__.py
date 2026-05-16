34530a74dbec0a4d85f1e2a70fd00d427ccd3ee3
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-16 02:12:44 -0300

    BLINGFIX cria pacote universal MapeiaAI

---
+++ bling_app_zero/universal/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from bling_app_zero.universal.model_detector import detect_model_type
+from bling_app_zero.universal.output_builder import build_universal_output
+
+__all__ = ['build_universal_output', 'detect_model_type']
