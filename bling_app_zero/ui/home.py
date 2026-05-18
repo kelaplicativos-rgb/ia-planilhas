24046e59b6dea5a0e02f58d5deba5823f6ad9a04
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-18 14:06:05 -0300

    Enable global attention alert policy on app home

--- bling_app_zero/ui/home.py
+++ bling_app_zero/ui/home.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from bling_app_zero.ui.alerts import enforce_attention_alert_policy
 from bling_app_zero.ui.home_router import render_home_router
 from bling_app_zero.ui.layout import inject_app_layout, render_compact_hero
 from bling_app_zero.ui.scroll_position import inject_scroll_position_keeper
@@ -9,6 +10,7 @@ from bling_app_zero.ui.wizard_state_guard import run_wizard_state_guard
 def render_home() -> None:
     from bling_app_zero.ui.home_autofluxo import run_home_autofluxo
 
+    enforce_attention_alert_policy()
     run_wizard_state_guard()
     run_home_autofluxo()
     inject_app_layout()
