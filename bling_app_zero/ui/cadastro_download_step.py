a2e341d0b1aa40e95f36a38fdf585fdd64d37a33
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-18 22:27:45 -0300

    BLINGFIX: show useful Bling links after final download

--- bling_app_zero/ui/cadastro_download_step.py
+++ bling_app_zero/ui/cadastro_download_step.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import streamlit as st
 
+from bling_app_zero.ui.bling_links_panel import render_bling_links_panel
 from bling_app_zero.ui.cadastro_wizard_state import (
     enforce_cadastro_model_columns,
     render_row_count_blocker,
@@ -23,6 +24,8 @@ def render_cadastro_download_step() -> None:
         return
 
     download_final(df_final, 'modelo', 'modelo_anexado')
+    st.divider()
+    render_bling_links_panel()
 
 
 __all__ = ['render_cadastro_download_step']
