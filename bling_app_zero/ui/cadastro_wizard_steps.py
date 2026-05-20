7797441fa55f1fdab17c3cb0b8a7a41c2f64c1cc
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-19 22:18:57 -0300

    BLINGCLEAN: criar aliases universais para etapas

--- bling_app_zero/ui/cadastro_wizard_steps.py
+++ bling_app_zero/ui/cadastro_wizard_steps.py
@@ -6,6 +6,15 @@ from bling_app_zero.ui.cadastro_mapping_step import render_cadastro_mapeamento_s
 from bling_app_zero.ui.cadastro_preview_step import render_cadastro_preview_step
 from bling_app_zero.ui.cadastro_wizard_state import cadastro_context_ready, cadastro_mapping_ready
 
+# Aliases universais: preservam compatibilidade com os módulos antigos de cadastro,
+# mas permitem que o fluxo novo use nomes alinhados ao modelo final universal.
+universal_context_ready = cadastro_context_ready
+universal_mapping_ready = cadastro_mapping_ready
+render_universal_download_step = render_cadastro_download_step
+render_universal_entrada_step = render_cadastro_entrada_step
+render_universal_mapeamento_step = render_cadastro_mapeamento_step
+render_universal_preview_step = render_cadastro_preview_step
+
 
 __all__ = [
     'cadastro_context_ready',
@@ -14,4 +23,10 @@ __all__ = [
     'render_cadastro_entrada_step',
     'render_cadastro_mapeamento_step',
     'render_cadastro_preview_step',
+    'universal_context_ready',
+    'universal_mapping_ready',
+    'render_universal_download_step',
+    'render_universal_entrada_step',
+    'render_universal_mapeamento_step',
+    'render_universal_preview_step',
 ]
