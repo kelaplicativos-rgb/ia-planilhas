88a2ba3f3795e01fdc900d0986e2248f6f272048
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-25 18:21:05 -0300

    BLINGFIX: remover links do download estoque

--- bling_app_zero/ui/estoque_download_step.py
+++ bling_app_zero/ui/estoque_download_step.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import streamlit as st
 
-from bling_app_zero.ui.bling_links_panel import render_bling_links_panel
 from bling_app_zero.ui.estoque_outputs import render_stock_downloads
 from bling_app_zero.ui.estoque_wizard_state import build_stock_outputs_if_possible
 
@@ -15,7 +14,6 @@ def render_estoque_download_step() -> None:
         st.warning('Ainda não há planilha de estoque. Volte para o mapeamento do estoque.')
         return
     render_stock_downloads()
-    render_bling_links_panel()
 
 
 __all__ = ['render_estoque_download_step']
