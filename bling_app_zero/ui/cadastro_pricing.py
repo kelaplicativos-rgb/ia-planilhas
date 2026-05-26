3a430a93048c2027c4e27233bd9330a89e99a433
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-26 19:21:55 -0300

    BLINGMODULAR 2: corrigir chave da origem precificada

--- bling_app_zero/ui/cadastro_pricing.py
+++ bling_app_zero/ui/cadastro_pricing.py
@@ -15,6 +15,7 @@ from bling_app_zero.ui.home_shared import df_signature
 PRICED_SOURCE_KEY = 'df_origem_cadastro_precificada'
 PRICING_ACTIVE_KEY = 'cadastro_preco_calculado_ativo'
 PRICING_SOURCE_KEY = 'shared_price_calculator_source'
+CADASTRO_ORIGEM_PRICED_STATE_KEY = 'cadastro_wizard_df_para_mapear'
 
 
 def apply_calculated_price_aliases(df: pd.DataFrame, calculated_column: str = 'Preço de venda') -> pd.DataFrame:
@@ -38,7 +39,7 @@ def _store_pricing_state(signature: str, selected_cost_column: str) -> None:
 def clear_cadastro_pricing_state() -> None:
     st.session_state[PRICING_ACTIVE_KEY] = False
     st.session_state.pop(PRICED_SOURCE_KEY, None)
-    st.session_state.pop('cadastro_origem_priced_key', None)
+    st.session_state.pop(CADASTRO_ORIGEM_PRICED_STATE_KEY, None)
 
 
 def apply_cadastro_pricing(df_origem: pd.DataFrame, *, channel: str = 'etapa_preco') -> pd.DataFrame:
@@ -70,6 +71,7 @@ def apply_cadastro_pricing(df_origem: pd.DataFrame, *, channel: str = 'etapa_pre
     origem_signature = df_signature(df_origem)
     _store_pricing_state(origem_signature, result.source_column)
     st.session_state[PRICED_SOURCE_KEY] = result.df
+    st.session_state[CADASTRO_ORIGEM_PRICED_STATE_KEY] = result.df
     return result.df
 
 
