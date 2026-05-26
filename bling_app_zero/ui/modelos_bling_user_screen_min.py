f4923af2093465f4af818daec94b4be2dcff00a2
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-26 00:12:11 -0300

    BLINGFIX: add model store import

--- bling_app_zero/ui/modelos_bling_user_screen_min.py
+++ bling_app_zero/ui/modelos_bling_user_screen_min.py
@@ -2,10 +2,13 @@ from __future__ import annotations
 
 import streamlit as st
 
+from bling_app_zero.ui.user_bling_models_store import MODEL_TYPES
+
 
 def render_modelos_bling_user_screen() -> None:
     st.markdown('### Bling')
     st.info('Tela de modelos base do usuario.')
+    st.write(MODEL_TYPES)
 
 
 __all__ = ['render_modelos_bling_user_screen']
