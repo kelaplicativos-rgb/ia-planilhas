b9f4c02efb9659950a7d9b32218b53e695c21556
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-27 18:19:56 -0300

    Add universal wizard state alias

---
+++ bling_app_zero/ui/universal_wizard_state.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.cadastro_wizard_state import (
+    CADASTRO_MODELO_KEY as UNIVERSAL_MODELO_KEY,
+    CADASTRO_ORIGEM_KEY as UNIVERSAL_ORIGEM_KEY,
+    CADASTRO_ORIGEM_PRICED_KEY as UNIVERSAL_ORIGEM_PRICED_KEY,
+    cadastro_context_ready as universal_context_ready,
+    cadastro_mapping_ready as universal_mapping_ready,
+    clear_cadastro_outputs_if_source_changed as clear_universal_outputs_if_source_changed,
+    is_site_origin,
+    store_cadastro_context as store_universal_context,
+    valid_df,
+    valid_model,
+)
+
+__all__ = [
+    'UNIVERSAL_MODELO_KEY',
+    'UNIVERSAL_ORIGEM_KEY',
+    'UNIVERSAL_ORIGEM_PRICED_KEY',
+    'clear_universal_outputs_if_source_changed',
+    'is_site_origin',
+    'store_universal_context',
+    'universal_context_ready',
+    'universal_mapping_ready',
+    'valid_df',
+    'valid_model',
+]
