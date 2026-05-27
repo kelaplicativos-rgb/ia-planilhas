18acacd88c65b6ec263088662249313bf852fd06
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-27 18:19:25 -0300

    Add universal preview step alias

---
+++ bling_app_zero/ui/universal_preview_step.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.cadastro_preview_step import render_cadastro_preview_step as render_universal_preview_step
+
+__all__ = ['render_universal_preview_step']
