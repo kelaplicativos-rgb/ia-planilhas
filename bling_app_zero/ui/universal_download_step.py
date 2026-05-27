344ee414a47f24ad31a62c0491b1629cb324bf03
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-27 18:19:38 -0300

    Add universal download step alias

---
+++ bling_app_zero/ui/universal_download_step.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.cadastro_download_step import render_cadastro_download_step as render_universal_download_step
+
+__all__ = ['render_universal_download_step']
