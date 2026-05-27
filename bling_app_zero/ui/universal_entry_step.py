848f3442525e3e957754566dcccac72f1e61958a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-27 18:18:53 -0300

    Add universal entry step alias

---
+++ bling_app_zero/ui/universal_entry_step.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.cadastro_entry_step import (
+    empty_cadastro_upload_result as empty_universal_upload_result,
+    render_cadastro_entrada_step as render_universal_entrada_step,
+    site_source_dataframe,
+    source_dataframe,
+)
+
+__all__ = [
+    'empty_universal_upload_result',
+    'render_universal_entrada_step',
+    'site_source_dataframe',
+    'source_dataframe',
+]
