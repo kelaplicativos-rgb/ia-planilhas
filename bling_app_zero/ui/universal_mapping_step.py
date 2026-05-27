0bac07ae8e986b13b8c8d9a20a29dffc78e77011
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-27 18:19:12 -0300

    Add universal mapping step alias

---
+++ bling_app_zero/ui/universal_mapping_step.py
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+from bling_app_zero.ui.cadastro_mapping_step import render_cadastro_mapeamento_step as render_universal_mapeamento_step
+
+__all__ = ['render_universal_mapeamento_step']
