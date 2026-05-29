fe2d6c21607e4dc6c69a3ec89e10b961d9299b74
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-05-29 17:32:20 -0300

    Força deploy do BLINGFIX token envio direto

--- bling_app_zero/core/app_config.py
+++ bling_app_zero/core/app_config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-APP_VERSION = '3.7.3-BLINGFIX-CONEXAO-BLING'
+APP_VERSION = '3.7.4-BLINGFIX-TOKEN-ENVIO-DIRETO'
 
 PAGE_CONFIG = {
     'page_title': 'MapeiaAI',
