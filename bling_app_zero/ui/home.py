6fd1d56a893a9872baa9ab47e18eb9979cc5dec6
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-05 17:08:52 -0300

    BLINGFIX: corrigir import do home router

--- bling_app_zero/ui/home.py
+++ bling_app_zero/ui/home.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from bling_app_zero.ui.alerts import enforce_attention_alert_policy
 from bling_app_zero.ui.bottom_nav import render_bottom_nav
-from bling_app_zero.ui.home_router import render_home_router
+from bling_app_zero.ui.home_router import render_home as render_home_router
 from bling_app_zero.ui.layout import inject_app_layout, render_compact_hero
 from bling_app_zero.ui.scroll_position import inject_scroll_position_keeper
 from bling_app_zero.ui.wizard_state_guard import run_wizard_state_guard
