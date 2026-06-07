e8d220a873f090a9407ddf0fa381069a4dfda3b6
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-07 10:47:06 -0300

    BLINGFIX: criar sender verificado corrigido

---
+++ bling_app_zero/core/verified_api_sender_blingfix.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from bling_app_zero.core.verified_api_sender import send_verified_products as _original_send_verified_products
+from bling_app_zero.core.bling_pre_send_defaults import apply_dataframe_send_defaults
+from bling_app_zero.core.blingfix_verified_runtime_patch import apply_blingfix_to_verified_module
+
+RESPONSIBLE_FILE = 'bling_app_zero/core/verified_api_sender_blingfix.py'
+
+
+def send_verified_products(df, *, limit=None, progress_callback=None):
+    from bling_app_zero.core import verified_api_sender
+
+    apply_blingfix_to_verified_module(verified_api_sender)
+    fixed_df = apply_dataframe_send_defaults(df)
+    return _original_send_verified_products(fixed_df, limit=limit, progress_callback=progress_callback)
+
+
+__all__ = ['RESPONSIBLE_FILE', 'send_verified_products']
