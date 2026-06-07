8fbd8aed27e5e15bc95a25353f6fd3f98e04dcf0
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-07 10:56:47 -0300

    Cria sender verificado com nome técnico neutro

---
+++ bling_app_zero/core/verified_api_sender_guarded.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from bling_app_zero.core.verified_api_sender_blingfix import send_verified_products
+
+RESPONSIBLE_FILE = 'bling_app_zero/core/verified_api_sender_guarded.py'
+
+__all__ = ['RESPONSIBLE_FILE', 'send_verified_products']
