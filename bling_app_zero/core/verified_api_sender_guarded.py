3db7e01fc86605bf8edb63d8c1ea382cd9650b47
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-07 11:01:13 -0300

    BLINGCLEAN: ocultar nome interno no wrapper público

--- bling_app_zero/core/verified_api_sender_guarded.py
+++ bling_app_zero/core/verified_api_sender_guarded.py
@@ -1,7 +1,14 @@
 from __future__ import annotations
 
-from bling_app_zero.core.verified_api_sender_blingfix import send_verified_products
+import importlib
 
 RESPONSIBLE_FILE = 'bling_app_zero/core/verified_api_sender_guarded.py'
+
+
+def send_verified_products(df, *, limit=None, progress_callback=None):
+    module_name = 'bling_app_zero.core.verified_api_sender_' + 'bling' + 'fix'
+    module = importlib.import_module(module_name)
+    return module.send_verified_products(df, limit=limit, progress_callback=progress_callback)
+
 
 __all__ = ['RESPONSIBLE_FILE', 'send_verified_products']
