5b62f558b6823b7997ac4f1c9ef3e38046d41bd6
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-08 17:34:55 -0300

    BLINGFIX: ativar progresso global no pacote ui

--- bling_app_zero/ui/__init__.py
+++ bling_app_zero/ui/__init__.py
@@ -1,3 +1,9 @@
 """Interfaces Streamlit do BLINGCREATOR."""
 
+try:
+    from bling_app_zero.ui.live_operation_runtime_patch import install_live_operation_runtime_patch
+    install_live_operation_runtime_patch()
+except Exception:
+    pass
+
 __all__ = []
