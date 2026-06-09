657a85daa0805e14c1a643061d3e6d869060fc15
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-09 16:40:30 -0300

    Update app version

--- bling_app_zero/core/app_config.py
+++ bling_app_zero/core/app_config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-APP_VERSION = '3.8.9-BLINGFIX-ESTOQUE-URL-BLOCK-20260609'
+APP_VERSION = '3.9.0-FLUXO-LIMPO-20260609'
 
 PAGE_CONFIG = {
     'page_title': 'MapeiaAI',
