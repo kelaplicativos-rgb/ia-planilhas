394742f146d77e9d87150508f9c984ed15c46d2a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-09 15:38:02 -0300

    BLINGFIX: bump version for stock site API fix

--- bling_app_zero/core/app_config.py
+++ bling_app_zero/core/app_config.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-APP_VERSION = '3.8.5-BLINGFIX-ROTA-ATIVA-20260609'
+APP_VERSION = '3.8.6-BLINGFIX-ESTOQUE-SITE-API-20260609'
 
 PAGE_CONFIG = {
     'page_title': 'MapeiaAI',
