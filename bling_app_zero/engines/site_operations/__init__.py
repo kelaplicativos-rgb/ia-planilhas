c928139ea32a04cf4524397dc7d673d84104b06f
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-10 23:33:35 -0300

    Exporta motor central de busca por site

--- bling_app_zero/engines/site_operations/__init__.py
+++ bling_app_zero/engines/site_operations/__init__.py
@@ -1,5 +1,25 @@
 from __future__ import annotations
 
-from bling_app_zero.engines.site_operations.router import run_site_operation_engine
+from bling_app_zero.engines.site_operations.central_engine import (
+    ProductSiteSearchPlan,
+    build_product_site_search_plan,
+    normalize_operation,
+    normalize_site_operation,
+    run_cadastro_site_engine,
+    run_estoque_site_engine,
+    run_product_site_search,
+    run_site_operation_engine,
+    run_universal_site_engine,
+)
 
-__all__ = ['run_site_operation_engine']
+__all__ = [
+    'ProductSiteSearchPlan',
+    'build_product_site_search_plan',
+    'normalize_operation',
+    'normalize_site_operation',
+    'run_cadastro_site_engine',
+    'run_estoque_site_engine',
+    'run_product_site_search',
+    'run_site_operation_engine',
+    'run_universal_site_engine',
+]
