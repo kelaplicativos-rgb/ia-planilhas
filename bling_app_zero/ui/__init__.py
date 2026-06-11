3de6e3286c63c9c90399c04bed36eaf2ed51c908
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-11 09:04:34 -0300

    Conecta fix de download de estoque no boot

--- bling_app_zero/ui/__init__.py
+++ bling_app_zero/ui/__init__.py
@@ -6,4 +6,10 @@ try:
 except Exception:
     pass
 
+try:
+    from bling_app_zero.ui.download_estoque_runtime_fix import install_download_estoque_runtime_fix
+    install_download_estoque_runtime_fix()
+except Exception:
+    pass
+
 __all__ = []
