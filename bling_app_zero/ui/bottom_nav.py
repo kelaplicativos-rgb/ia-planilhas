38972bdffe8bd16b5c3a954bb6a3536c796bd07e
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-15 14:12:52 -0300

    BLINGFIX: exportar controles persistentes leves

--- bling_app_zero/ui/bottom_nav.py
+++ bling_app_zero/ui/bottom_nav.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-from bling_app_zero.ui.bottom_nav_modern import render_bottom_nav
+from bling_app_zero.ui.bottom_nav_modern import render_bottom_nav, render_persistent_operation_controls
 
 RESPONSIBLE_FILE = 'bling_app_zero/ui/bottom_nav.py'
 
-__all__ = ['render_bottom_nav']
+__all__ = ['render_bottom_nav', 'render_persistent_operation_controls']
