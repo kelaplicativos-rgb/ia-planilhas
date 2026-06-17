6442de3d2119f0aaa8d233a4db88b67f8db1d75a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-17 16:00:34 -0300

    BLINGFIX: add site cadastro engine compatibility module

---
+++ bling_app_zero/engines/site_cadastro_engine.py
@@ -0,0 +1,7 @@
+from __future__ import annotations
+
+from bling_app_zero.engines.site_operations.cadastro_engine import run_cadastro_site_engine
+
+RESPONSIBLE_FILE = 'bling_app_zero/engines/site_cadastro_engine.py'
+
+__all__ = ['RESPONSIBLE_FILE', 'run_cadastro_site_engine']
