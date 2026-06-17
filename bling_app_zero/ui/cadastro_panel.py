a56df0901a4dc296b2976fbd132b7a4e770d20a3
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-17 16:07:11 -0300

    BLINGFIX: point cadastro panel shim to current entry function

--- bling_app_zero/ui/cadastro_panel.py
+++ bling_app_zero/ui/cadastro_panel.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
-from bling_app_zero.ui.cadastro_entry_step import render_cadastro_entry_step
+from bling_app_zero.ui.cadastro_entry_step import render_cadastro_entrada_step
 
 RESPONSIBLE_FILE = 'bling_app_zero/ui/cadastro_panel.py'
 
 
 def render_cadastro_panel() -> None:
-    render_cadastro_entry_step()
+    render_cadastro_entrada_step()
 
 
 __all__ = ['RESPONSIBLE_FILE', 'render_cadastro_panel']
