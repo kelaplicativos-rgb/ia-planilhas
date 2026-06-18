8685a9c3d35fb2ccdb100be6743a786826fadcd5
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-18 00:26:17 -0300

    Atualizar etapa universal

--- bling_app_zero/ui/universal_download_step.py
+++ bling_app_zero/ui/universal_download_step.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from bling_app_zero.ui.cadastro_download_step import render_cadastro_download_step
+from bling_app_zero.ui.cadastro_download_step_v2 import render_cadastro_download_step
 
 
 def render_universal_download_step() -> None:
