67fce44d2fd36f3bea3666945ab040ef2eab688a
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-23 10:13:25 -0300

    BLINGFIX: criar patch rapido universal modelo

---
+++ bling_app_zero/core/universal_model_upload_fast_patch.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from typing import Any
+
+RESPONSIBLE_FILE = 'bling_app_zero/core/universal_model_upload_fast_patch.py'
+
+
+def install_universal_model_upload_fast_patch() -> None:
+    return None
+
+
+__all__ = ['install_universal_model_upload_fast_patch']
