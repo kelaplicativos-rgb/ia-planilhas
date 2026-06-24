1c2dc9e8ab57f55830bbccfb6d6d82be5a6eb8fb
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-24 08:57:53 -0300

    BLINGFIX: ativar patch do modelo multiloja

--- bling_app_zero/core/__init__.py
+++ bling_app_zero/core/__init__.py
@@ -2,6 +2,12 @@
 
 from __future__ import annotations
 
+try:
+    from bling_app_zero.core import multiloja_price_model_patch as _multiloja_price_model_patch
+    _multiloja_price_model_patch.install()
+except Exception:
+    pass
+
 from bling_app_zero.core.app_config import APP_VERSION, PAGE_CONFIG
 from bling_app_zero.core.app_errors import register_critical_error
 
