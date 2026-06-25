125549d23c930aa8ce96365f5683459ad656881b
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-24 21:31:09 -0300

    BLINGFIX: testa falsos positivos do scan de categorias

--- tests/test_category_finalizer_blingfix.py
+++ tests/test_category_finalizer_blingfix.py
@@ -26,6 +26,17 @@ def test_power_bank_nao_vira_carregador_celular():
     assert report['forced'] >= 2
 
 
+def test_pilha_mah_nao_vira_power_bank():
+    categories, _report = _run([
+        {
+            'Descrição': 'Pilhas Recarregáveis AA 2700mAh',
+            'Descrição Complementar': 'Kit com pilhas AA recarregaveis 2700mAh.',
+            'Categoria do produto': 'Pilhas e baterias',
+        },
+    ])
+    assert categories == ['Pilhas e baterias']
+
+
 def test_radio_nao_vira_caixa_de_som():
     categories, _report = _run([
         {
@@ -42,11 +53,33 @@ def test_radio_nao_vira_caixa_de_som():
     assert categories == ['Rádios AM e FM', 'Rádios AM e FM']
 
 
+def test_radio_com_entrada_fone_nao_vira_fone():
+    categories, _report = _run([
+        {
+            'Descrição': 'Radio AM/FM com entrada de fone',
+            'Descrição Complementar': 'Radio portatil com antena telescopica, entrada para fone e USB.',
+            'Categoria do produto': 'Fones de ouvido',
+        },
+    ])
+    assert categories == ['Rádios AM e FM']
+
+
+def test_caixa_de_som_com_microfone_nao_vira_microfone():
+    categories, _report = _run([
+        {
+            'Descrição': 'Caixa de Som Portátil 20W com Microfones',
+            'Descrição Complementar': 'Caixa de som amplificada bluetooth com microfone incluso.',
+            'Categoria do produto': 'Microfones',
+        },
+    ])
+    assert categories == ['Caixas de som']
+
+
 def test_cabo_rede_nao_vira_categoria_errada():
     categories, _report = _run([
         {
             'Descrição': 'Cabo de Rede 5M Hmaston CR-5',
-            'Descrição Complementar': 'Cabo RJ45 CAT6 Ethernet LAN para internet.',
+            'Descrição Complementar': 'Cabo RJ-45 CAT6 Ethernet LAN para internet.',
             'Categoria do produto': 'Projetores',
         },
     ])
@@ -59,6 +92,17 @@ def test_fone_nao_vira_mouse():
             'Descrição': 'Fone de Ouvido Intra-auricular',
             'Descrição Complementar': 'Fone intra auricular com microfone para celular.',
             'Categoria do produto': 'Mouses',
+        },
+    ])
+    assert categories == ['Fones de ouvido']
+
+
+def test_fone_curto_nao_vira_microfone():
+    categories, _report = _run([
+        {
+            'Descrição': 'Fone Gamer com Microfone Integrado',
+            'Descrição Complementar': 'Fone bluetooth gamer com microfone para chamadas.',
+            'Categoria do produto': 'Microfones',
         },
     ])
     assert categories == ['Fones de ouvido']
@@ -113,6 +157,17 @@ def test_cabo_tipo_c_continua_cabo_usb():
         {
             'Descrição': 'Cabo Tipo C para iPhone 1 Metro',
             'Descrição Complementar': 'Cabo de dados tipo c lightning para celular.',
+            'Categoria do produto': '',
+        },
+    ])
+    assert categories == ['Cabos USB e dados']
+
+
+def test_rca_nao_bate_dentro_de_palavra_reforcado():
+    categories, _report = _run([
+        {
+            'Descrição': 'Cabo reforcado USB 2 metros',
+            'Descrição Complementar': 'Cabo usb tipo c para dados.',
             'Categoria do produto': '',
         },
     ])
