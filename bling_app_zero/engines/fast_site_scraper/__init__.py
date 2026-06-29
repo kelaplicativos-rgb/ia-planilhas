2cf06c1fc02c6f56fd7a91f8050cc104a650b4ca
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-29 18:15:35 -0300

    Instala runtime WBuy de paginacao

--- bling_app_zero/engines/fast_site_scraper/__init__.py
+++ bling_app_zero/engines/fast_site_scraper/__init__.py
@@ -5,5 +5,8 @@ from bling_app_zero.engines.fast_site_scraper.wbuy_cards_patch import install as
 _install_wbuy_cards_patch()
 
 from bling_app_zero.engines.fast_site_scraper.engine import run_fast_site_scraper
+from bling_app_zero.engines.fast_site_scraper.wbuy_live_runtime import install as _install_wbuy_live_runtime
+
+_install_wbuy_live_runtime()
 
 __all__ = ['run_fast_site_scraper']
