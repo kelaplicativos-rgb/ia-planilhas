7ea200579ee2de8c653a646e5a7d1e1f276f47ba
kelaplicativos-rgb <kel.aplicativos@gmail.com>
2026-06-29 17:45:28 -0300

    Instala patch WBuy antes do scraper rápido

--- bling_app_zero/engines/fast_site_scraper/__init__.py
+++ bling_app_zero/engines/fast_site_scraper/__init__.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 
+from bling_app_zero.engines.fast_site_scraper.wbuy_cards_patch import install as _install_wbuy_cards_patch
+
+_install_wbuy_cards_patch()
+
 from bling_app_zero.engines.fast_site_scraper.engine import run_fast_site_scraper
 
 __all__ = ['run_fast_site_scraper']
